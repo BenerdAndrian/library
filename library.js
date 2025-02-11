9ab05582c0fa904bdef97f43c09f7f2c0565410c
BenerdAndrian <ben527466@gmail.com>
2025-02-11 16:18:48 +0000

    Adding function to add book into library array
    instantiate some datas(book) for library

--- library.js
+++ library.js
@@ -7,3 +7,12 @@ function Book(bookName,bookAuthor,bookPages,status){
     this.bookPages=bookPages;
     this.status=status;
 }
+// add Book to library function
+function addBookToLibrary(book){
+    library.push(book);
+}
+// instantiate some books for library
+const book1=new Book("Lonely Wolf","Silver lin",266,"not read");
+const book2=new Book("The Rain Upside Down","Michael Nots",418,"not read");
+const book3=new Book("By The Dawn","Selena Gomez",188,"not read");
+const book4=new Book("A Freakin' Pretty Girl","Indiana Gospel",317,"read");
