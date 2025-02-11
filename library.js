2f8cc094c99d04d0411369233f913eea9b97ffdc
BenerdAndrian <ben527466@gmail.com>
2025-02-11 16:12:29 +0000

    Adding object constructor Book

---
+++ library.js
@@ -0,0 +1,9 @@
+const library=[];
+
+//construction object
+function Book(bookName,bookAuthor,bookPages,status){
+    this.bookName=bookName;
+    this.bookAuthor=bookAuthor;
+    this.bookPages=bookPages;
+    this.status=status;
+}
