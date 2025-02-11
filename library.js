e47bc811494996896acca244573cccf5c62d93f1
BenerdAndrian <ben527466@gmail.com>
2025-02-11 16:28:20 +0000

    Adding function to display library data
    Using DOM to manipulate it in HTML and render the data out

--- library.js
+++ library.js
@@ -11,8 +11,30 @@ function Book(bookName,bookAuthor,bookPages,status){
 function addBookToLibrary(book){
     library.push(book);
 }
+//display Library data
+function displayLibrary(){
+    const theLib=document.querySelector(".library");
+    theLib.innerHTML=``;
+    for(let i=0;i<library.length;i++){
+        const list=document.createElement("li");
+        list.setAttribute("class","book");
+        list.setAttribute("data-index",i);
+        list.innerHTML=`<h3>${library[i].bookName}<h3>
+                        <p>${library[i].bookAuthor}</p>
+                         <p>${library[i].bookPages}</p>
+                          <button>${library[i].status}</button>
+        `
+        theLib.appendChild(list);
+    }
+}
 // instantiate some books for library
 const book1=new Book("Lonely Wolf","Silver lin",266,"not read");
 const book2=new Book("The Rain Upside Down","Michael Nots",418,"not read");
 const book3=new Book("By The Dawn","Selena Gomez",188,"not read");
 const book4=new Book("A Freakin' Pretty Girl","Indiana Gospel",317,"read");
+//add to library array
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+addBookToLibrary(book4);
+displayLibrary();
